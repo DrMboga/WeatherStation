2108e62f9fc9caf6f1c8744234abdc92db51d0c5
Michael Shabanov <dr.mboga@yahoo.com>
2024-11-30 19:47:01 +0100

    feat: multiplexer plus LCD formatting

--- Esp32/src/main.cpp
+++ Esp32/src/main.cpp
@@ -2,8 +2,16 @@
 #include <Wire.h>
 #include <LiquidCrystal.h>
 
+#define ITERATIONS_FOR_SCREEN_CHANGE (9)
+bool isTemperatureShowing = true;
+int currentIteration = 0;
+
+// DHT11
 int pinDHT11 = 15;
 SimpleDHT11 dht11(pinDHT11);
+
+// TCA multiplexer
+#define TCAADDR (0x70)
 
 // Addresses for all GY-21 sensors
 #define GY21_ADDRESS (0x40)
@@ -104,7 +112,27 @@ float gy21ReadHumidity(uint8_t address) {
   return humidity;
 }
 
+// --- Multiplexer
+void TCA9548A(uint8_t bus) {
+  Wire.beginTransmission(0x70);
+  Wire.write(1 << bus);
+  Wire.endTransmission();
+}
 // ---
+
+int lcdCalculateStartColumn(int value) {
+  // 2 digit value or one digit with minus sign
+  if ((value >= 10 && value < 100)  ||
+    (value < 0 && value > -10)) {
+    return 1;
+  }
+  // One digit value
+  if ((value >= 0 && value < 10)) {
+    return 2;
+  }
+  // 3 digit value or 2 digit with minus sign
+  return 0;
+}
 
 void setup() { 
   Serial.begin(115200);
@@ -113,6 +141,16 @@ void setup() {
 
   Wire.begin();
 
+  Serial.println("Switching multiplexer to first GY-21 and resetting the sensor");
+  TCA9548A(0);
+  gy21Reset(GY21_ADDRESS);
+
+  Serial.println("Switching multiplexer to second GY-21 and resetting the sensor");
+  TCA9548A(1);
+  gy21Reset(GY21_ADDRESS);
+
+  Serial.println("Switching multiplexer to third GY-21 and resetting the sensor");
+  TCA9548A(2);
   gy21Reset(GY21_ADDRESS);
 }
 
@@ -121,34 +159,78 @@ void loop() {
   float dht11Humidity;
   readDht11Data(dht11Temperature, dht11Humidity);
 
+  // Switching multiplexer to first GY-21
+  TCA9548A(0);
   float gy211Temperature = gy21ReadTemperature(GY21_ADDRESS);
   float gy211Humidity = gy21ReadHumidity(GY21_ADDRESS);
 
-  lcd.setCursor(0, 0);
-  lcd.print("DHT11:");
-  lcd.setCursor(7, 0);
-  lcd.print((int)dht11Temperature);
-  lcd.setCursor(9, 0);
-  lcd.print((char)223); // Celsius symbol
-  lcd.setCursor(10, 0);
-  lcd.print(",");
-  lcd.setCursor(12, 0);
-  lcd.print((int)dht11Humidity);
-  lcd.setCursor(14, 0);
-  lcd.print("%");
-  
-  lcd.setCursor(0, 1);
-  lcd.print("GY-21:");
-  lcd.setCursor(7, 1);
-  lcd.print((int)gy211Temperature);
-  lcd.setCursor(9, 1);
-  lcd.print((char)223); // Celsius symbol
-  lcd.setCursor(10, 1);
-  lcd.print(",");
-  lcd.setCursor(12, 1);
-  lcd.print((int)gy211Humidity);
-  lcd.setCursor(14, 1);
-  lcd.print("%");
+  // Switching multiplexer to scond GY-21
+  TCA9548A(1);
+  float gy212Temperature = gy21ReadTemperature(GY21_ADDRESS);
+  float gy212Humidity = gy21ReadHumidity(GY21_ADDRESS);
+
+  // Switching multiplexer to third GY-21
+  TCA9548A(2);
+  float gy213Temperature = gy21ReadTemperature(GY21_ADDRESS);
+  float gy213Humidity = gy21ReadHumidity(GY21_ADDRESS);
+
+  // Show result
+  currentIteration++;
+  if (ITERATIONS_FOR_SCREEN_CHANGE == currentIteration) {
+    currentIteration = 0;
+    isTemperatureShowing = !isTemperatureShowing;
+    lcd.clear();
+  }
+
+  lcd.setCursor(4, 0);
+  lcd.print("|");
+  lcd.setCursor(4, 1);
+  lcd.print("|");
+  lcd.setCursor(11, 1);
+  lcd.print("|");
+  if (isTemperatureShowing) {
+    lcd.setCursor(lcdCalculateStartColumn((int)dht11Temperature), 0);
+    lcd.print((int)dht11Temperature);
+    lcd.setCursor(3, 0);
+    lcd.print((char)223); // Celsius symbol
+
+    lcd.setCursor(lcdCalculateStartColumn((int)gy211Temperature), 1);
+    lcd.print((int)gy211Temperature);
+    lcd.setCursor(3, 1);
+    lcd.print((char)223); // Celsius symbol
+
+    lcd.setCursor(6 + lcdCalculateStartColumn((int)gy212Temperature), 1);
+    lcd.print((int)gy212Temperature);
+    lcd.setCursor(9, 1);
+    lcd.print((char)223); // Celsius symbol
+
+    lcd.setCursor(12 + lcdCalculateStartColumn((int)gy213Temperature), 1);
+    lcd.print((int)gy213Temperature);
+    lcd.setCursor(15, 1);
+    lcd.print((char)223); // Celsius symbol
+
+  }
+  else {
+    lcd.setCursor(lcdCalculateStartColumn((int)dht11Humidity), 0);
+    lcd.print((int)dht11Humidity);
+    lcd.setCursor(3, 0);
+    lcd.print("%");
+
+    lcd.setCursor(lcdCalculateStartColumn((int)gy211Humidity), 1);
+    lcd.print((int)gy211Humidity);
+    lcd.setCursor(3, 1);
+    lcd.print("%");
+
+    lcd.setCursor(6 + lcdCalculateStartColumn((int)gy212Humidity), 1);
+    lcd.print((int)gy212Humidity);
+    lcd.setCursor(9, 1);
+    lcd.print("%");
+
+    lcd.setCursor(12 + lcdCalculateStartColumn((int)gy213Humidity), 1);
+    lcd.print((int)gy213Humidity);
+    lcd.setCursor(15, 1);
+    lcd.print("%");
+  }
 
   Serial.print("DHT11 temperature: ");
   Serial.print((int)dht11Temperature);
@@ -158,6 +240,14 @@ void loop() {
   Serial.print((int)gy211Temperature);
   Serial.print("°, ");
   Serial.print((int)gy211Humidity);
+  Serial.print("% | GY-21 No 2: ");
+  Serial.print((int)gy212Temperature);
+  Serial.print("°, ");
+  Serial.print((int)gy212Humidity);
+  Serial.print("% | GY-21 No 3: ");
+  Serial.print((int)gy213Temperature);
+  Serial.print("°, ");
+  Serial.print((int)gy213Humidity);
   Serial.println("%");
 
   delay(1500); // DHT11 sampling rate is 1HZ.
